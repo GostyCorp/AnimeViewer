216b3d9480a5920e6015e646364fc780dc40d406
VeryOldpApY <VeryOldpApY@pm.me>
2023-10-26 02:36:00 +0200

    Ajouter le suivi de la progression des épisodes à EpisodeFicheView
    Cet engagement remplace la classe UserControl par la classe AnimeViewer.Models. Un contrôle ProgressBar est inclus dans le XAML, caché par défaut, utilisé pour afficher la progression du visionnage. Deux méthodes privées, GetProgress et EpisodeFicheView_OnLoaded ont également été ajoutées. La méthode GetProgress récupère la progression du visionnage dans l'historique, met à jour les valeurs de la barre de progression et définit sa visibilité. La méthode EpisodeFicheView_OnLoaded est attachée à l'événement de sortie du contrôle Loading pour s'assurer que la barre de progression est mise à jour chaque fois que le contrôle EpisodeFicheView est chargé.

--- Pages/EpisodeFicheView.xaml.cs
+++ Pages/EpisodeFicheView.xaml.cs
@@ -1,9 +1,9 @@
 using System.Windows;
-using System.Windows.Controls;
+using AnimeViewer.Models;
 
 namespace AnimeViewer.Pages
 {
-	public partial class EpisodeFicheView : UserControl
+	public partial class EpisodeFicheView
 	{
         private readonly MainWindow _main;
 		public EpisodeFicheView()
@@ -15,6 +15,22 @@ namespace AnimeViewer.Pages
 		private void Button_OnClick(object sender, RoutedEventArgs e)
 		{
 			_main?.CreatePlayer(id: (int)IdNumber.Value);
+		}
+
+		private void GetProgress()
+		{
+			History history = History.GetHistory(Episode.GetEpisode((int)IdNumber.Value));
+			Progress.Value = history?.Time ?? 0;
+			Progress.Maximum = history?.MaxTime ?? 100;
+			if(Progress.Value != 0)
+			{
+				Progress.Visibility = Visibility.Visible;
+			}
+		}
+
+		private void EpisodeFicheView_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			GetProgress();
 		}
 	}
 }
