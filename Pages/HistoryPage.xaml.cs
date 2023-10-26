45f6f67b8f817dcf3f8e625b9903a87a76c10fb3
VeryOldpApY <VeryOldpApY@pm.me>
2023-10-26 08:24:35 +0200

    Suppression des fichiers relatifs à l'utilisateur, ajout de la page d'historique
    Le modèle User, UserPage.xaml et UserPage.xaml.cs ont été supprimés car ils ne sont plus nécessaires dans notre projet. Nous avons déplacé la gestion des informations sur les utilisateurs vers un mécanisme différent, ce qui a rendu ces fichiers obsolètes.
    
    En remplacement, une nouvelle page d'historique a été ajoutée à l'application. Cette page est conçue pour afficher l'historique de visionnage d'un anime. Elle est composée d'une liste dynamique qui récupère les données du modèle d'historique et les affiche. La page est développée pour améliorer l'expérience de l'utilisateur en lui permettant de suivre les animes qu'il a visionnés.

---
+++ Pages/HistoryPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using AnimeViewer.Models;
+
+namespace AnimeViewer.Pages
+{
+	/// <summary>
+	/// Logique d'interaction pour userPage.xaml
+	/// </summary>
+	public partial class HistoryPage
+	{
+		private readonly ObservableCollection<Serie> _series = new ObservableCollection<Serie>();
+
+		public HistoryPage()
+		{
+			InitializeComponent();
+			SerieListView.DataContext = _series;
+		}
+
+		public void LoadHistory()
+		{
+			List<History> histories = History.GetListHistory();
+			_series.Clear();
+			foreach(History history in histories)
+			{
+				if(_series.All(serie => serie.Id != history.Episode.Serie.Id))
+				{
+					_series.Add(history.Episode.Serie);
+				}
+			}
+		}
+	}
+}
